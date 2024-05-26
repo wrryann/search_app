71acb1f531eeed78384eabffd20bd3e75ea780c3
wrryann <wrryan@gmail.com>
2024-05-26 17:21:41 -0300

    + pokemons + better search method

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,24 @@
 use std::io;
+use pokemon_rs;
 fn main() {
-        let v1 = vec!["sla oq", "2", "3", "bla bla", "42", "45", "64", "86"];
-        let mut v1_iter = v1.into_iter();
+        loop {
+                println!("type break to stop");
+                println!("search any pokemon:");
+                let pokemon_names = pokemon_rs::get_all(None);
 
-        let mut user_input = String::new();
-        io::stdin().read_line(&mut user_input)
-            .expect("sla");
+                let mut user_input = String::new();
+                io::stdin().read_line(&mut user_input)
+                .expect("can't read input :C, try again");
+                // |
+                if user_input.trim().to_uppercase() == "BREAK" { break; }
 
-        let total = v1_iter.find(|&x| x == user_input.trim());
+                for pokemon in pokemon_names {
+                        if pokemon.trim().to_uppercase().contains(&user_input.trim().to_uppercase()){
+                                println!("{pokemon}");
 
-        match total {
-                Some(value) => println!("{}", value),
-                None => println!("{:?}", total)
+                        } else {
+                                continue;
+                        }
+                }
         }
-
 }
